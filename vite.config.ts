a302217aa2b8827d2617b4fd0a7e0f14a6144ca6
Abdullah ElMetwali <abdullahelmetwali@icloud.com>
2024-04-24 18:07:52 +0200

    ViteBaseChanges

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://github.com/abdullahelmetwali/ShopCoECommerce/',
+  base: '/ShopCoECommerce/',
   plugins: [vue(), vueJsx(), VueDevTools()],
   resolve: {
     alias: {
