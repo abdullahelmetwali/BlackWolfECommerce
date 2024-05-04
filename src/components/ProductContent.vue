e44deebd5592397a46532c158e3f17efaa677a2b
Abdullah ElMetwali <abdullahelmetwali@icloud.com>
2024-05-05 00:05:32 +0200

    NewFeaturesDist

--- src/components/ProductContent.vue
+++ src/components/ProductContent.vue
@@ -18,12 +18,6 @@ export default {
         GetSalePrice(price, salePercentage) {
             return getSalePrice(price, salePercentage)
         },
-        ChangeRouter() {
-            this.$emit('SeenProduct');
-            this.$emit('goToUp');
-            this.$router.push(`/${theDetails.theStyle}/${theTitle.replaceAll(' ', '')}`);
-
-        }
     },
     props: ['theMainImg', 'theStyle', 'theTitle', 'isSeen', 'theDetails', 'showColor', 'theColors', 'theSizes', 'thePrice', 'salePercentage'],
     emits: ['toggleColor', 'SeenProduct', 'setItems', 'goToUp'],
@@ -32,36 +26,33 @@ export default {
 
 
 <template>
-    <RouterLink :to="`/${theDetails.theStyle}/${theTitle.replaceAll(' ', '')}`"
-        @click="this.$emit('SeenProduct'), this.$emit('goToUp')">
+    <div>
         <div>
-            <div>
-                <img :src="theMainImg" class="w-full h-[80vh] img">
-                <div class="onLoad cursor-pointer absolute right-4 bottom-20 bg-[#080808e8] w-fit py-3 px-4 mob:bottom-[7rem]"
-                    :class="{ getOut: !isSeen }">
-                    <Bag @click="isSeen = !isSeen" />
-                </div>
+            <img :src="theMainImg" class="w-full h-[80vh] img">
+            <div class="onLoad cursor-pointer absolute right-4 bottom-20 bg-[#080808e8] w-fit py-3 px-4 mob:bottom-[7rem]"
+                :class="{ getOut: !isSeen }">
+                <Bag @click="isSeen = !isSeen" />
             </div>
-            <div>
-                <div class="flex w-full justify-between px-14 bg-[#080808e8] absolute bottom-16 mob:bottom-[7rem]"
-                    :class="{ getIn: !isSeen }">
-                    <div v-for="Size in theDetails.theSizes" :key="Size"
-                        class="size cursor-pointer py-1 px-2  hover:bg-[#302f2fe8]" v-show="!isSeen"
-                        :class="{ hidden: showColor }" @click="selectedSize = Size, showColor = !showColor">
-                        <p>
-                            {{ Size }}
-                        </p>
-                    </div>
-                    <div v-for="Color in theDetails.theColors" :key="Color"
-                        class="hidden cursor-pointer py-1 px-2  hover:bg-[#302f2fe8]" :class="{ getIn: showColor }">
-                        <p
-                            @click="selectedColor = Color, this.$emit('setItems', this.selectedSize, this.selectedColor), this.$emit('toggleColor')">
-                            {{ Color }} </p>
-                    </div>
+        </div>
+        <div>
+            <div class="flex w-full justify-between px-14 bg-[#080808e8] absolute bottom-16 mob:bottom-[7rem]"
+                :class="{ getIn: !isSeen }">
+                <div v-for="Size in theDetails.theSizes" :key="Size"
+                    class="size cursor-pointer py-1 px-2  hover:bg-[#302f2fe8]" v-show="!isSeen"
+                    :class="{ hidden: showColor }" @click="selectedSize = Size, showColor = !showColor">
+                    <p>
+                        {{ Size }}
+                    </p>
+                </div>
+                <div v-for="Color in theDetails.theColors" :key="Color"
+                    class="hidden cursor-pointer py-1 px-2  hover:bg-[#302f2fe8]" :class="{ getIn: showColor }">
+                    <p
+                        @click="selectedColor = Color, this.$emit('setItems', this.selectedSize, this.selectedColor), this.$emit('toggleColor')">
+                        {{ Color }} </p>
                 </div>
             </div>
         </div>
-    </RouterLink>
+    </div>
     <div class=" w-[17rem] text-center text-nowrap">
         <RouterLink class="text-nowrap" :to="`/${theDetails.theStyle}/${theTitle.replaceAll(' ', '')}`"
             @click="this.$emit('SeenProduct'), this.$emit('goToUp')">
