801f4a98fef7c2ea05d07e45a34ae4a9b706b2a3
Abdullah ElMetwali <abdullahelmetwali@icloud.com>
2024-07-30 06:37:12 +0300

    Base Changes

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/BlackWolfECommerce/',
+  base: '/',
   plugins: [vue(), vueJsx(), VueDevTools()],
   resolve: {
     alias: {
